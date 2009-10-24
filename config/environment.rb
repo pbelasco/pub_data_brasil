8b21b26fa93f9767ff9b405eec640699cf532cc8
Pedro Belasco <pbelasco@gmail.com>
2009-10-24 16:34:35 -0200

    unpacking gems & alterando env

--- config/environment.rb
+++ config/environment.rb
@@ -17,9 +17,14 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "hpricot", :version => '0.8.1'
+  config.gem "curb", :version => '0.5.4.0'
+  config.gem "capistrano", :version => '2.5.8'
+  config.gem "capistrano-ext", :version => '1.2.1'
+  config.gem "ferret", :version => "0.11.6"
+  config.gem "acts_as_ferret", :version => "0.4.4"
+  config.gem "mysql", :version => "2.7"
+
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -34,7 +39,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = 'Brasilia'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
