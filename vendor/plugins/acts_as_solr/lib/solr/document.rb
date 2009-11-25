d25b0ee65a64d2c14b76fcb6a8347fbc648a3610
Pedro Belasco <pbelasco@gmail.com>
2009-11-25 12:13:14 -0200

    solr

--- vendor/plugins/acts_as_solr/lib/solr/document.rb
+++ vendor/plugins/acts_as_solr/lib/solr/document.rb
@@ -12,6 +12,8 @@
 
 require 'solr/xml'
 require 'solr/field'
+require 'extended_string'
+
 ILLEGAL_XML_CHARS = /\x00|\x01|\x02|\x03|\x04|\x05|\x06|\x07|\x08|\x0B|
 \x0C|\x0E|\x0F|\x10|\x11|\x12|\x13|\x14|\x15|\x16|\x17|\x18|\x19|\x1A|
 \x1B|\x1C|\x1D|\x1E|\x1F/
@@ -72,7 +74,7 @@ class Solr::Document
     e = Solr::XML::Element.new 'doc'
     e.attributes['boost'] = @boost.to_s if @boost
     @fields.each do |f| 
-      f.value = f.value.gsub(ILLEGAL_XML_CHARS, '')
+      f.value = f.value.gsub(ILLEGAL_XML_CHARS, '').strip_diacritics
       e.add_element(f.to_xml)
     end
     return e
