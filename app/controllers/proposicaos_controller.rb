2ee823dba4b32f0a7cc494fbd5b279857766be95
Pedro Belasco <pbelasco@gmail.com>
2009-12-25 12:20:13 -0200

    Correcao no auto-update

--- app/controllers/proposicaos_controller.rb
+++ app/controllers/proposicaos_controller.rb
@@ -5,7 +5,15 @@ class ProposicaosController < ApplicationController
   layout '1col'
   
   def index
-   
+    sort = case params['sort']
+              when "data"   then "apresentacao DESC"
+              when "data_reverse" then "apresentacao ASC"
+                
+              when "name_reverse"  then "name DESC"
+              when "qty_reverse"   then "quantity DESC"
+              when "price_reverse" then "price DESC"
+    end
+              
     @num_proposicaos = Proposicao.count(:all)
     @proposicaos = Proposicao.paginate(:page => params[:page], :per_page => 10, :order => 'apresentacao DESC', :include => :andamentos)
     @proposicaos.each do |p| 
@@ -29,11 +37,6 @@ class ProposicaosController < ApplicationController
       
       @proposicaos = Proposicao.find_by_solr("#{params[:q].strip_diacritics}", {:limit => 10, :offset => params[:page] })
       
-      @proposicaos.docs.each do |p| 
-        if p.ellegible_for_update 
-          Delayed::Job.enqueue(UpdateCamaraProposition.new( p.id_sileg), 2) 
-        end
-      end
       
       respond_to do |format|
         format.html # index.html.erb
@@ -51,11 +54,6 @@ class ProposicaosController < ApplicationController
   # GET /proposicaos/1.xml
   def show
     @proposicao = Proposicao.find(params[:id], :include => :andamentos)
-    
-    if @proposicao.ellegible_for_update 
-      flash[:notice] = "A proposição que você está visualizando pode não estar atualizada. Uma atualização do conteúdo já foi agendada..."
-      Delayed::Job.enqueue(UpdateCamaraProposition.new(params[@proposicao.id_sileg]), 1)
-    end
     
     respond_to do |format|
       format.html # show.html.erb
@@ -76,69 +74,4 @@ class ProposicaosController < ApplicationController
       
     end
   end
-
-  # GET /proposicaos/new
-  # GET /proposicaos/new.xml
-  # def new
-  #   @proposicao = Proposicao.new
-  # 
-  #   respond_to do |format|
-  #     format.html # new.html.erb
-  #     format.xml  { render :xml => @proposicao }
-  #     format.json { render :json => @proposicao.to_json }
-  #     format.yaml { render :yaml => @proposicao.to_yaml }
-  #     
-  #   end
-  # end
-
-  # GET /proposicaos/1/edit
-  # def edit
-  #   @proposicao = Proposicao.find(params[:id])
-  # end
-
-  # POST /proposicaos
-  # POST /proposicaos.xml
-  # def create
-  #   @proposicao = Proposicao.new(params[:proposicao])
-  # 
-  #   respond_to do |format|
-  #     if @proposicao.save
-  #       flash[:notice] = 'Proposicao was successfully created.'
-  #       format.html { redirect_to(@proposicao) }
-  #       format.xml  { render :xml => @proposicao, :status => :created, :location => @proposicao }
-  #     else
-  #       format.html { render :action => "new" }
-  #       format.xml  { render :xml => @proposicao.errors, :status => :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PUT /proposicaos/1
-  # PUT /proposicaos/1.xml
-  # def update
-  #   @proposicao = Proposicao.find(params[:id])
-  # 
-  #   respond_to do |format|
-  #     if @proposicao.update_attributes(params[:proposicao])
-  #       flash[:notice] = 'Proposicao was successfully updated.'
-  #       format.html { redirect_to(@proposicao) }
-  #       format.xml  { head :ok }
-  #     else
-  #       format.html { render :action => "edit" }
-  #       format.xml  { render :xml => @proposicao.errors, :status => :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /proposicaos/1
-  # DELETE /proposicaos/1.xml
-  # def destroy
-  #   @proposicao = Proposicao.find(params[:id])
-  #   @proposicao.destroy
-  # 
-  #   respond_to do |format|
-  #     format.html { redirect_to(proposicaos_url) }
-  #     format.xml  { head :ok }
-  #   end
-  # end
 end
